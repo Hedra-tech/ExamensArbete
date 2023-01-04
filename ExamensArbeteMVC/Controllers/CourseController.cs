09ca496b15009f683777c68d00644653ff2f31b5
Hedra Gourieh <hedragourieh@hotmail.com>
2023-01-04 20:04:12 +0100

    Upload project files

---
+++ ExamensArbeteMVC/Controllers/CourseController.cs
@@ -0,0 +1,30 @@
+﻿using ExamensArbeteMVC.Data;
+using ExamensArbeteMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExamensArbeteMVC.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly CourseData _courseData = null;
+            public CourseController()
+        {
+            _courseData = new CourseData();
+        }
+        public List<CourseModel> GetAllCourses()
+        {
+            return _courseData.GetAllCourses();
+        }
+
+        public CourseModel GetCourseById(int id)
+        {
+            return _courseData.GetCourseById(id);
+        }
+
+
+        public List<CourseModel> SearchCourses (string title, string teacherName)
+        {
+            return _courseData.SearchCourses(title, teacherName);
+        }
+    }
+}
---
+++ ExamensArbeteMVC/Models/CourseModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ExamensArbeteMVC.Models
+{
+    public class CourseModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Teacher { get; set; }
+    }
+}
